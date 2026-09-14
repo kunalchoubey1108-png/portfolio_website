8794750c2cd86e5a8d65d4509b40e973fbbad208
Kunal <kunalchoubey1108@gmail.com>
2026-09-14 17:21:13 +0530

    Add basePath configuration for GitHub Pages subdirectory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "/portfolio_website",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
